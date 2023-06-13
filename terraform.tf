49986c0c5e9ce3630764a3a59acae4646529d267
josephnjel <66813517+josephnjel@users.noreply.github.com>
2023-06-13 22:11:00 +0000

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,13 @@
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  # cloud {
+  #   # organization = "organization-name"
+  #   organization = "nexah"
 
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
+  #   workspaces {
+  #     name = "learn-terraform-cloud"
+  #   }
+  # }
 
   required_providers {
     aws = {
